166ab07d193730e484edf1620b5806338a17a8d5
istamosh <63596969+istamosh@users.noreply.github.com>
2024-09-16 21:38:43 +0700

    fixing permissions, routes should be working now but mysql container is stopped and react's npm run dev is having permission issues

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
+// define routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
