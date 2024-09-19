4ce65e15414022854b8240ce5f39a8d54b038679
istamosh <63596969+istamosh@users.noreply.github.com>
2024-09-19 14:39:20 +0700

    functionalizing loigin page, need to show specific error message from axios

--- react/src/views/Login.tsx
+++ react/src/views/Login.tsx
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
 
 const Login: React.FC = () => {
+    const emailRef = useRef<HTMLInputElement>();
+    const passwordRef = useRef<HTMLInputElement>();
+
+    const [errors, setErrors] = useState(null);
+    // when you press CTRL+SPACE the state context will show
+    // from ContextProvider
+    const { setUser, setToken } = useStateContext();
+
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const payload = {
+            email: emailRef.current ? emailRef.current.value : "",
+            password: passwordRef.current ? passwordRef.current.value : "",
+        };
+
+        console.log(payload);
+        // get { data } response from AuthController
+        axiosClient
+            .post("/login", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((err) => {
+                console.log(err);
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors);
+                }
+            });
     };
 
     return (
         <>
             <form action="" onSubmit={onSubmit}>
                 <h1 className="title">Login into your account</h1>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                {/* display errors */}
+                {errors && (
+                    <div className="alert">
+                        {Object.keys(errors).map((key) => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                )}
+                <input
+                    ref={emailRef as React.RefObject<HTMLInputElement>}
+                    type="email"
+                    placeholder="Email"
+                />
+                <input
+                    ref={passwordRef as React.RefObject<HTMLInputElement>}
+                    type="password"
+                    placeholder="Password"
+                />
                 <button className="btn btn-block">Login</button>
                 <p className="message">
                     Not registered?{" "}
