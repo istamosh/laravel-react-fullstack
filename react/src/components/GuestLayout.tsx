0d66d9c6a402eea9176bc34767c64cd6a32d7caf
istamosh <63596969+istamosh@users.noreply.github.com>
2024-09-26 15:53:05 +0700

    styling guest page

--- react/src/components/GuestLayout.tsx
+++ react/src/components/GuestLayout.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { Button, DarkThemeToggle, Flowbite, Navbar } from "flowbite-react";
 
 const GuestLayout: React.FC = () => {
     const { token } = useStateContext();
-    // debugger;
+
+    // if user is already logged in, redirect to home page
     if (token) {
         return <Navigate to="/" />;
     }
 
     return (
-        <div>
-            <div className="login-signup-form animated fadeInDown">
-                <div className="form">
+        <>
+            <div className="bg-gray-100 dark:bg-gray-900">
+                <Navbar fluid rounded className="mb-3">
+                    <Navbar.Brand href="https://flowbite-react.com">
+                        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                            Welcome, Guest!
+                        </span>
+                    </Navbar.Brand>
+                    <div className="flex md:order-2">
+                        <Button>Get started</Button>
+                        <Flowbite>
+                            <DarkThemeToggle className="ml-3" />
+                        </Flowbite>
+                    </div>
+                </Navbar>
+                <div className="animated fadeInDown px-4 py-2">
                     <Outlet />
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
