6fd37c3c8b5fc638db0a29d981d8266c1c74a349
istamosh <63596969+istamosh@users.noreply.github.com>
2024-09-28 08:57:59 +0700

    refresh the db, added admin, now want to put /users only for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,20 +5,28 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     * @return void
      */
     public function run(): void
     {
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
+
+        // seed a default user
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@doe',
+            'password' => Hash::make('password!'),
+        ]);
+
         // seed a total of 50 users
         User::factory()->count(50)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
